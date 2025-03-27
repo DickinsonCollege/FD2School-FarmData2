21d7704efd2f9bdb548ee568d669a83b4bc3bcd9
AdacusG <greenead@dickinson.edu>
2025-03-27 09:39:21 -0400

    Created tests to confirm correct farm name, username, and language.

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generation.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generation.spec.js
@@ -11,4 +11,14 @@ describe("Test the generated harvest report values", () => {
         cy.get("[data-cy=report-header]")
             .should("be.visible")
     })
+
+    it("Testing correctness of information", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm-name]")
+            .should("have.text", "Farm:Sample Farm")
+        cy.get("[data-cy=username]")
+            .should("contain.text", "manager1")
+        cy.get("[data-cy=language]")
+            .should("have.text", "English")
+    })
 })
