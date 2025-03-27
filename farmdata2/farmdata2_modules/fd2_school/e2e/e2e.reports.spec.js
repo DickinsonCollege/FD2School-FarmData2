0c36f7b8176c4bdf85c91a2fe8d64a6468d463dc
HoangPhung-11 <80802358+HoangPhung-11@users.noreply.github.com>
2025-03-26 22:08:24 -0400

    Language test done

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.reports.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.reports.spec.js
@@ -10,5 +10,21 @@ describe("Test the harvest report default values", () => {
         cy.get('[data-cy="report-header"]').should('be.visible');
     })
 
+    it('Checks that the farm name is correctly displayed when the report is generated', () => {
+        // Click the "Generate Report" button
+        cy.get('[data-cy="generate-report-button"]').click();
+    
+        // Check that the farm name is displayed correctly
+        cy.get('[data-cy="farm-name"]').should('be.visible').and('contain.text', 'Sample Farm');
+    });
+    
+    it('Checks that the language is correctly displayed when the report is generated', () => {
+        // Click the "Generate Report" button
+        cy.get('[data-cy="generate-report-button"]').click();
+    
+        // Check that the language is displayed correctly
+        cy.get('[data-cy="language"]').should('have.text', 'English');
+    });
+    
     
 })
