5c9a98ee81e2ec9b8b815d5868e7fbe1c0fd93b6
HoangPhung-11 <80802358+HoangPhung-11@users.noreply.github.com>
2025-03-26 21:32:57 -0400

    Crop name test finished

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -16,5 +16,13 @@ describe("Test the harvest report default values", () => {
 
     })
 
+    it("Check the crop names", () => {
+        cy.get('[data-cy="crop-dropdown"]').children().eq(0).should('have.text', 'ARUGULA');
+        cy.get('[data-cy="crop-dropdown"]').children().eq(4).should('have.text', 'BEAN-FAVA'); 
+        cy.get('[data-cy="crop-dropdown"]').children().last().should('have.text', 'ZUCCHINI');
+        cy.get('[data-cy="crop-dropdown"]').children().should('have.length', 111);
+        
+    })
+
     
 })
