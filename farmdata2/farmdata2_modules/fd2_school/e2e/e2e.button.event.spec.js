c5ef5f34704c926ff81699e4137a248ec416fa6d
btaniemie <lennminh@gmail.com>
2025-03-30 19:09:51 -0400

    check the report details after clicking the button

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.button.event.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.button.event.spec.js
@@ -5,7 +5,7 @@ describe("Test the generate report button click event", () => {
     })
 
     it("Check that the report title does not exist before clicking the button", () => {
-        // cy.get("[data-cy=report-title]").should("not exist")
+        // cy.get("[data-cy=report-title]").should("not.exist")
     })
       
     it("Check the page header", () => {
@@ -14,6 +14,14 @@ describe("Test the generate report button click event", () => {
 
     it("Check that the report title is visible after clicking the button", () => {
         cy.get("[data-cy=report-title]").should("be.visible")
+    })
+
+    it("Check report details after clicking the button", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm]").should("have.text","Farm:Sample Farm")
+        cy.get("[data-cy=user]").should("contain.text","manager1")
+        cy.get("[data-cy=language]").should("have.text","Language:English")
+  
     })
 
 })
