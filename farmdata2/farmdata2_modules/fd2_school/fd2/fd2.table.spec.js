d6277a176fc2bc8c8eaef322f05bb8cc5c0b96c2
Lukas Hamm <hamml@dickinson.edu>
2025-04-02 18:27:58 -0400

    Test table headers
    Signed-off-by: Lukas Hamm <hamml@dickinson.edu>

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -5,7 +5,13 @@ describe("Test the report generation", () => {
     })
 
     it("Check the report title exists", () => {
-      cy.get("[data-cy=report-title]").should("exist")
+	cy.get("[data-cy=generate-report-button]").click()
+	cy.get("[data-cy=h0]").should("have.text","Date")
+        cy.get("[data-cy=h1]").should("have.text","Area")
+        cy.get("[data-cy=h2]").should("have.text","Crop")
+        cy.get("[data-cy=h3]").should("have.text","Yield")
+        cy.get("[data-cy=h4]").should("have.text","Units")
+
     })
 
 
