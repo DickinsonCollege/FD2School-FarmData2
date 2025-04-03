fc672d84e2a04077b106c397873ff57138c54684
AdacusG <greenead@dickinson.edu>
2025-04-03 09:02:07 -0400

    Updated fd2.defaults.spec.js to work with fd2 tab.

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -1,7 +1,7 @@
 describe("Test the harvest report default values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
       
     it("Check the page header", () => {
@@ -17,14 +17,14 @@ describe("Test the harvest report default values", () => {
     })
 
     it("Check crop values in dropdown", () => {
-        cy.get("[data-cy=crop-values]").children().eq(0)
+        cy.get("[data-cy=option1]")
             .should("have.text","ARUGULA")
-        cy.get("[data-cy=crop-values]").children().eq(4) 
+        cy.get("[data-cy=option5]")
             .should("have.text","BEAN-FAVA")
-        cy.get("[data-cy=crop-values]").children().eq(110)    
+        cy.get("[data-cy=option111]")
             .should("have.text","ZUCCHINI")
-        cy.get("[data-cy=crop-values]").children()
-            .should("have.length",111)   
+        cy.get("[data-cy=dropdown-input]").children()
+            .should("have.length",112)   
 
     })
 })
