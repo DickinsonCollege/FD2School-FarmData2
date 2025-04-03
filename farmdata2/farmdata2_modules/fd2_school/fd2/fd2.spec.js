85c37b2bc5aaced5e863d80a7cb97555258dc14b
pediconj <pediconj@dickinson.edu>
2025-04-03 08:28:56 -0400

    tests the dropdown with the 'all' option

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.spec.js
@@ -14,9 +14,10 @@ describe("Test the harvest report default values", () => {
     })
 
     it("Check the crop dropdown", () => {
-        cy.get("[data-cy=crop-selector]").children().eq(0).should("have.value", "ARUGULA")
-        cy.get("[data-cy=crop-selector]").children().eq(4).should("have.value", "BEAN-FAVA")
-        cy.get("[data-cy=crop-selector]").children().eq(110).should("have.value", "ZUCCHINI")
-        cy.get("[data-cy=crop-selector]").children().should("have.length", 111)
+        cy.get("[data-cy=crop-selector] > [data-cy=dropdown-input] > [data-cy=option0]").should("have.text", "All")
+        cy.get("[data-cy=crop-selector] > [data-cy=dropdown-input] > [data-cy=option1]").should("have.text", "ARUGULA")
+        cy.get("[data-cy=crop-selector] > [data-cy=dropdown-input] > [data-cy=option5]").should("have.text", "BEAN-FAVA")
+        cy.get("[data-cy=crop-selector] > [data-cy=dropdown-input] > [data-cy=option111]").should("have.text", "ZUCCHINI")
+        cy.get("[data-cy=crop-selector] > [data-cy=dropdown-input]").children().should("have.length", 112)
     })
 })
