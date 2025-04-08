2e1cc2866d7a05b3700749f59584075ffad789b3
AzizbekMuminoff <40713295+AzizbekMuminoff@users.noreply.github.com>
2025-04-08 02:06:45 -0400

    Added tests to e2e sub tab

--- farmdata2/farmdata2_modules/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/e2e/e2e.defaults.spec.js
@@ -15,4 +15,28 @@ describe("Test the harvest report default values", () => {
       cy.get("[data-cy=end-date]")
           .should("have.value", "2020-05-15")
   })
+
+  it("Check the crop dropdown options", () => {
+      cy.get("[data-cy=crop-dropdown]")
+  })
+  it("Check the crop dropdown options", () => {
+    cy.get("[data-cy=crop-dropdown]")
+        .children()
+        .eq(0)
+        .should("have.text", "ARUGULA")
+
+    cy.get("[data-cy=crop-dropdown]")
+        .children()
+        .eq(4)
+        .should("have.text", "BEAN-FAVA")
+
+    cy.get("[data-cy=crop-dropdown]")
+        .children()
+        .last() 
+        .should("have.text", "ZUCCHINI")
+
+    cy.get("[data-cy=crop-dropdown]")
+        .children()
+        .should("have.length", 111)
+    })
 })
