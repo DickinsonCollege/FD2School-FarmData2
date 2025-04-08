ea997ba12ddc567115e9f4ff07e5624165a330c5
kieran <santosk@dickinson.edu>
2025-04-08 01:01:42 -0400

    created table spec.js

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,12 @@
+describe("Table", () => {
+
+    beforeEach(() => {
+      cy.login("manager1", "farmdata2"); 
+      cy.visit("/farm/fd2-school/fd2.");
+    });
+  
+    it("passes", () => {
+      
+    });
+  
+  });
