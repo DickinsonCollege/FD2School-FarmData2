a5fe92d7fec181e7274e0fb5c18d6011a3607f73
bussm <bussm@dickinson.edu>
2025-05-05 02:54:56 -0400

    Edited "e2e.generate.report.spec.js" to add a test for the farm name, user name, and language displayed on the E2E sub-tab.

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generate.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generate.report.spec.js
@@ -9,4 +9,11 @@ describe('Test the harvest report "Generate Report" button', () => {
         cy.get("[data-cy=generate-report-button]").click()
         cy.get("[data-cy=report-title").should("be.visible")
     })
+
+    it("Check the name of the farm, user, and language", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm-name").should("have.text", "Farm:Sample Farm")
+        cy.get("[data-cy=user-name").should("contain.text", "manager1")
+        cy.get("[data-cy=language").should("have.text", "en")
+    })
 })
