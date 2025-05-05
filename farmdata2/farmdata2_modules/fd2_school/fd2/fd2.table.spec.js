9afef2eb188015ee04fa767e8f64477dd5d15106
bussm <bussm@dickinson.edu>
2025-05-05 04:59:38 -0400

    Created "fd2.table.spec.js" to test the new CustomTableComponent in the FD2 sub-tab.

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,11 @@
+describe("Test the harvest report CustomTableComponent", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+    })
+      
+    it("Check the CustomTableComponent", () => {
+        
+    })
+
+})
